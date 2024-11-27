9d084373ee63211e8275a84d16e1ec643886049b
MarcuSantosDev <marcus.xtreme03@gmail.com>
2024-11-27 16:22:18 -0300

    :lipstick: feat text background

--- home/config.js
+++
@@ -1,8 +0,0 @@
-// TAparecer botão - clique
-
-const viewAllProductsLink = document.querySelector('.register-button');
-        const buttonsContainer = document.querySelector('.buttons-container');
-      
-        viewAllProductsLink.addEventListener('click', () => {
-          buttonsContainer.classList.toggle('active');
-        });
