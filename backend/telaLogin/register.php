f660ea69054fd328eb82439d484d01dc8353456d
joaoluca.s <joaolucasgc@icloud.com>
2024-12-05 01:00:02 -0300

    ✨ feat: enhance user registration process with validation and success message redirection.

--- backend/telaLogin/register.php
+++ backend/telaLogin/register.php
@@ -7,7 +7,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
 
     if (empty($name) || empty($email) || empty($password)) {
-        echo "Preencha todos os campos.";
+        echo "<script>alert('Preencha todos os campos'); window.location.href = '../../frontend/telaLogin/index.html';</script>";
+        exit;
+    }
+
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "<script>alert('Email ja cadastrado'); window.location.href = '../../frontend/telaLogin/index.html';</script>";
         exit;
     }
     
@@ -16,7 +24,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $query = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$hashedPassword')";
 
     if (mysqli_query($conn, $query)) {
-        echo "Cadastro realizado com sucesso!";
+        header("Location: ../../frontend/messages/RegisterSucess.html");
+        exit;
     } else {
         echo "Erro ao cadastrar: " . mysqli_error($conn);
     }
