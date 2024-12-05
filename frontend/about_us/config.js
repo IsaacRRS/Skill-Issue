7aa087eb192c2f9ffd9dc16987bf22f7b620a598
JoaoLucas <joaolucasgc@icloud.com>
2024-12-04 22:24:54 -0300

    🎨 style: update about us section and improve button visibility in home page.
    Co-authored-by: MarcuSantosDev <marcus.xtreme03@gmail.com>
    Co-authored-by: Isaac <161399527+IsaacRRS@users.noreply.github.com>

--- frontend/about_us/config.js
+++ frontend/about_us/config.js
@@ -1,12 +1,12 @@
+// Selecione os elementos
+const registerButton = document.getElementById('register-button');
+const registerButtonFilms = document.getElementById('register-button-films');
+const registerButtonSeries = document.getElementById('register-button-series');
+
 let sidebar = document.querySelector(".sidebar");
 let fechar = document.querySelector("#botao");
 
-// Adicione um evento de clique ao botão register-button
-registerButton.addEventListener('click', () => {
-  // Altere a propriedade visibility dos botões register-button-films e register-button-series
-  registerButtonFilms.style.visibility = 'visible';
-  registerButtonSeries.style.visibility = 'visible';
-});
+
 
 fechar.addEventListener("click", () => {
     sidebar.classList.toggle("open");
